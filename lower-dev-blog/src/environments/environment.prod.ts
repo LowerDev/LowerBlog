1555131d5ec65e787e97c7a9e671c0ed779f9b90
Rhodak <rhodak@Rhodaks-MacBook-Air.local>
2022-04-27 08:06:32 -0400

    Lower Dev Blog init

---
+++ lower-dev-blog/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'lower-dev-5d8d0',
+    appId: '1:679399764158:web:a5e6d373dd71453684eeef',
+    storageBucket: 'lower-dev-5d8d0.appspot.com',
+    apiKey: 'AIzaSyBGg4Ev6z8r75eEBBe2_-mKQAyC_iUxnps',
+    authDomain: 'lower-dev-5d8d0.firebaseapp.com',
+    messagingSenderId: '679399764158',
+    measurementId: 'G-52FVV02RPM',
+  },
+  production: true
+};
---
+++ lower-dev-blog/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'lower-dev-5d8d0',
+    appId: '1:679399764158:web:a5e6d373dd71453684eeef',
+    storageBucket: 'lower-dev-5d8d0.appspot.com',
+    apiKey: 'AIzaSyBGg4Ev6z8r75eEBBe2_-mKQAyC_iUxnps',
+    authDomain: 'lower-dev-5d8d0.firebaseapp.com',
+    messagingSenderId: '679399764158',
+    measurementId: 'G-52FVV02RPM',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
